c754b72c34862962c94e41296cfaa803ab11ba24
Abdur55 <skabdurrahim372@gmail.com>
2025-06-23 06:08:49 +0600

    this is my frist commit

---
+++ js/home.js
@@ -0,0 +1,11 @@
+document.getElementById('send-btn').addEventListener('click', function(event){
+      
+    event.preventDefault();
+
+    const accountnumber = getinputvaluebyid('user-account-number')
+    const transferamount = getinputvaluebyid('amount-of-transfer')
+    const transferpin = getinputvaluebyid('transfer-pin-number')
+
+   
+
+})
---
+++ js/utilities.js
@@ -0,0 +1,6 @@
+function  getinputvaluebyid(id){
+  const getinput =   document.getElementById(id).value
+  const getinputnumber = parseFloat(getinput)
+
+   return getinputnumber
+}
